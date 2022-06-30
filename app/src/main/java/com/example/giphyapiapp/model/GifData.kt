1588e5412d153cba398cc01becdd7eb3abfd382a
yaronero <yaroslavzakh.prog@gmail.com>
2022-06-30 15:52:52 +0300

    Created model, added permissions, layouts and implementations in gradle

---
+++ app/src/main/java/com/example/giphyapiapp/model/GifData.kt
@@ -0,0 +1,5 @@
+package com.example.giphyapiapp.model
+
+class GifData(
+    val gifUrl: String
+)
--- app/src/main/java/com/example/giphyapiapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/giphyapiapp/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.giphyapiapp
+package com.example.giphyapiapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.giphyapiapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
